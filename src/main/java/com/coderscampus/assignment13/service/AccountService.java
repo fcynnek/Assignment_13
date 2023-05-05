c8c5f1c36369494c88865ef1c867385c8055542b
fcynnek <kchengfung@gmail.com>
2023-05-04 21:00:53 -0400

    Working on Accounts

--- src/main/java/com/coderscampus/assignment13/service/AccountService.java
+++ src/main/java/com/coderscampus/assignment13/service/AccountService.java
@@ -1,5 +1,41 @@
 package com.coderscampus.assignment13.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.domain.User;
+import com.coderscampus.assignment13.repository.AccountRepository;
+
+@Service
 public class AccountService {
 
+	@Autowired
+	private AccountRepository accountRepo;
+	@Autowired
+	private UserService userService;
+	
+	public Account findAccountById (Long accountId) {
+		Optional<Account> accounts = accountRepo.findById(accountId);
+		
+		return accounts.orElse(new Account());
+	}
+	
+	public void save (Account account) {
+		accountRepo.save(account);
+	}
+	
+	public Account save (Long userId) {
+		User user = userService.findById(userId);
+		
+		Account account = new Account();
+		user.getAccounts().add(account);
+		account.getUsers().add(user);
+		account.setAccountName("Account" +  user.getAccounts());
+		
+		return accountRepo.save(account);
+	}
+	
 }
--- src/main/java/com/coderscampus/assignment13/web/AccountController.java
+++ src/main/java/com/coderscampus/assignment13/web/AccountController.java
@@ -1,9 +1,51 @@
 package com.coderscampus.assignment13.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.coderscampus.assignment13.domain.Account;
+import com.coderscampus.assignment13.domain.User;
+import com.coderscampus.assignment13.service.AccountService;
+import com.coderscampus.assignment13.service.UserService;
 
 @Controller
 public class AccountController {
 
+	@Autowired
+	private AccountService accountService;
+	@Autowired
+	private UserService userService;
 	
+	@GetMapping("users/{userId}/accounts/{accountId}")
+	public String getAccounts (ModelMap model, @PathVariable Long accountId) {
+		Account accounts = accountService.findAccountById(accountId);
+		User user = accounts.getUsers().get(0);
+		
+		model.put("accounts", accounts);
+		model.put("user", user);
+		
+		return "accounts";
+	}
+	
+	@PostMapping("users/{userId}/accounts")
+	public String postOneAccount (@PathVariable Long userId) {
+		accountService.save(userId);
+		
+		return "redirect:/users/" + userId;
+	}
+	
+	@PostMapping("users/{userId}/accounts/{accountId}") 
+	public String updateAccountName (@PathVariable Long userId, @PathVariable Long accountId, Account account) {
+		account.setAccountName(account.getAccountName());
+		accountService.save(account);
+		
+		userService.saveUser(userService.findById(userId));
+		
+		System.out.println(account.getAccountName());
+		return "redirect:/users/" + userId + "/accounts/" + accountId;
+	}
 }
